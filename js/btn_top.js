969e14b7ca04d477e577cc5291285f31adacc6da
giang10101 <58293871+giang10101@users.noreply.github.com>
2019-12-04 04:39:22 +0700

    up file lần 1

---
+++ js/btn_top.js
@@ -0,0 +1,17 @@
+﻿jQuery(document).ready(function ($) {
+    if ($(".btn-top").length > 0) {
+        $(window).scroll(function () {
+            var e = $(window).scrollTop();
+            if (e > 300) {
+                $(".btn-top").show()
+            } else {
+                $(".btn-top").hide()
+            }
+        });
+        $(".btn-top").click(function () {
+            $('body,html').animate({
+                scrollTop: 0
+            })
+        })
+    }
+});
